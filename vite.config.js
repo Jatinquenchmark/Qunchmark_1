a4f9fbde1f7cb8a253089a1700ee29e7ec3cfcf4
AmanMirza9898 <aman9811269898@gmail.com>
2026-01-04 10:57:59 +0530

    Updated base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Qunchmark_1/", // Aapke repo ka naam yahan hona chahiye
+  base: "/", // Aapke repo ka naam yahan hona chahiye
 })
